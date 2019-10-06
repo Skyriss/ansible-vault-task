c2c3f15f5129fb5d525ae51fac6941496132c999
Alex Glazkov <alex.farstone@gmail.com>
2019-10-06 16:58:03 +0300

    Add push nginx.conf and two vhosts

--- run_machine.tf
+++ run_machine.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 #Variables
 variable "do_token" {}
 variable "ssh_keyfile" {}
@@ -18,28 +22,14 @@ resource "digitalocean_droplet" "www" {
   name   = "webserver"
   region = "nyc3"
   size   = "s-1vcpu-1gb"
-  ssh_keys = ["${data.digitalocean_ssh_key.rebrain.id}","${digitalocean_ssh_key.local.id}"]
+  ssh_keys = [data.digitalocean_ssh_key.rebrain.id,digitalocean_ssh_key.local.id]
   tags = ["alex","glazkov","alexfarstone"]
-
-#  provisioner "local-exec" {
-#    command = "echo ${data.template_file.ansible_inventory.rendered} > invent.yml"
-#  }
 }
 
 # Deploy a SSH key
 resource "digitalocean_ssh_key" "local" {
   name       = "FarStone pub key"
-  public_key = "${file("${var.ssh_keyfile}")}"
-}
-
-data "template_file" "ansible_inventory" {
-  template = file("inventory.tpl")
-
-  vars = {
-    hostname = "${digitalocean_droplet.www.name}"
-    ip_address = "${digitalocean_droplet.www.ipv4_address}"
-  }
-  depends_on = [digitalocean_droplet.www]
+  public_key = file(var.ssh_keyfile)
 }
 
 resource "null_resource" "generate_inventfile" {
@@ -47,6 +37,16 @@ resource "null_resource" "generate_inventfile" {
     command = "echo \"${data.template_file.ansible_inventory.rendered}\" > invent.yml"
   }
 }
+
+data "template_file" "ansible_inventory" {
+  template = file("inventory.tpl")
+  vars = {
+    hostname = digitalocean_droplet.www.name
+    ip_address = digitalocean_droplet.www.ipv4_address
+  }
+  depends_on = [digitalocean_droplet.www]
+}
+
 output "instance_ipv4_addr" {
   value = digitalocean_droplet.www.ipv4_address
   description = "IP address of created VPS"
